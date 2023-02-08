111ba305bd0b4a9e513208c8f72b8fec58f0e53a
Neha Bhole <nehasbhole@gmail.com>
2023-02-08 13:17:56 -0500

    update repo

--- portfolio/src/components/Experience/Experience.js
+++
@@ -1,9 +0,0 @@
-import './Experience.scss';
-
-export default function Experience() {
-    return (
-        <section className='Experience' id='Experience'>
-            <h1>Experience</h1>
-        </section>
-    )
-}
