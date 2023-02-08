7e7506d36dffc42f11854fadeda8a1a806ba8b68
Neha Bhole <nehasbhole@gmail.com>
2023-02-08 11:14:28 -0500

    update repo

---
+++ src/components/App/App.js
@@ -0,0 +1,30 @@
+import './App.scss';
+//import { Routes, Route } from 'react-router-dom';
+import NavBar from '../NavBar/NavBar'
+import Home from '../Home/Home'
+import About from '../About/About'
+import Portfolio from '../Portfolio/Portfolio';
+import Skills from '../Skills/Skills';
+import Experience from '../Experience/Experience'
+import Contact from '../Contact/Contact'
+import Footer from '../Footer/Footer'
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <main>
+        <Home />
+        <About />
+        <Skills />
+        <Portfolio />
+        {/* <Experience /> */}
+        <Contact />
+        <Footer />
+      </main>
+    </div>
+  );
+}
+
+
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,17 @@
+import './Contact.scss';
+
+export default function Contact() {
+    return (
+        <section className='Contact' id='Contact'>
+            <div class="ContactMe">
+                <h1>Contact</h1>
+                <div>
+                    <p>I am currently looking for opportunities, </p>
+                    <p>I'd love chat about anything dev, learn about your journey or just simply hear from you!</p>
+                    <p>Connect with me via email!</p>
+                    <button>CONNECT</button>
+                </div>
+            </div>
+        </section>
+    )
+}
