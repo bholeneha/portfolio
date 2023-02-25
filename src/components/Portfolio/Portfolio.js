0e45ae6d577b9f07decb4910afaffcbf43247ec2
Neha Bhole <nehasbhole@gmail.com>
2023-02-24 22:24:24 -0500

    update links for projects

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -22,7 +22,7 @@ export default function Portfolio() {
                             <br /><br />
                             A fun and engaging web browser game that puts your short-term memory to the test! With each level, the sequence of light and sounds becomes longer, challenging players to remember and repeat the pattern. Developed using HTML, CSS, and JavaScript, this game is sure to provide hours of entertainment while also sharpening your memory skills. Give it a try and see how far you can go!
                         </p>
-                        <button><a>LIVE</a></button> <button><a>REPO</a></button>
+                        <button><a href='https://bholeneha.github.io/simon/'>LIVE</a></button> <button><a href='https://github.com/bholeneha/simon'>REPO</a></button>
                     </div>
                 </div>
 
@@ -36,7 +36,7 @@ export default function Portfolio() {
                             <br /><br />
                             Resource Vault is a user-friendly web application designed to help students find the resources they need to succeed. Built using MongoDB, Express, and Node.js, it allows users to browse a wide variety of categories to find the right resources for their needs. With the added convenience of Google OAuth login, users can easily log in and save their favorite links for quick and easy access later on. Explore the power of Resource Vault today and take your learning journey to the next level!
                         </p>
-                        <button><a>LIVE</a></button> <button><a>REPO</a></button>
+                        <button><a href='https://resource-vault.up.railway.app/'>LIVE</a></button> <button><a href='https://github.com/bholeneha/resource-vault'>REPO</a></button>
                     </div>
                     <div className='RVImg'>
                         <img width="500" src={rv} alt="Resource Vault Project Screen Shot" loading="lazy" />
@@ -57,7 +57,7 @@ export default function Portfolio() {
                             <br /><br />
                             An online plant shop that provides users with a wide variety of plants to explore and purchase through a shopping cart system. Built using the MERN stack, it offers a user-friendly interface and including secure user authentication through the use of JWT tokens. Users can easily sign up, log in, and have their carts automatically saved, and additional features to be added to enhance the overall user experience. Leaf it to me is a destination for all plant enthusiasts.
                         </p>
-                        <button><a>LIVE</a></button> <button><a>REPO</a></button>
+                        <button><a href='https://leafittome.herokuapp.com/'>LIVE</a></button> <button><a hred='https://leafittome.herokuapp.com/'>REPO</a></button>
                     </div>
                 </div>
             </div>
