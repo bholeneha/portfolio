2ad06f0ce20cbcc463f829ac2b9a3eb6e7c3d3b6
Neha Bhole <nehasbhole@gmail.com>
2023-04-08 10:53:43 -0400

    add chroma project

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,6 +2,7 @@ import './Portfolio.scss';
 import simon from '../../assets/images/simon.png'
 import rv from '../../assets/images/rv.png'
 import leafshop from '../../assets/images/leaf.png'
+import chroma from '../../assets/images/chroma.png'
 
 export default function Portfolio() {
     return (
@@ -9,14 +10,29 @@ export default function Portfolio() {
             <h1>Portfolio</h1>
             <br />
             <div className="Projects">
+
                 <div className='Project ProjectOne'>
-                    <div className='SimonImg'>
-                        <img width="500" src={simon} alt="Simon Project Screen Shot" loading="lazy" />
+                    <div className='chromaImg'>
+                        <img width="500" src={chroma} alt="Chroma Project Screen Shot" loading="lazy" />
                     </div>
                     <div>
-                        <h3>Simon</h3>
+                        <h3>chrōma</h3>
                         <br />
-                        <h4>HTML | CSS | JavaScript</h4>
+                        <h4> Node.js | Express.js | React | MongoDB </h4>
+                        <p>
+                            chrōma is a cosmetic color reference. Built using the MERN stack.
+                            <br /><br />
+                            An app designed to help people with color vision deficiency (CVD) better identify and understand the colors in makeup products. (This was a hackathon project)
+                        </p>
+                        <button><a href='https://chroma-cosmetics-color-ref.herokuapp.com/'>LIVE</a></button> <button><a href='https://github.com/cosmetics-ref-elc/cosmetics-color-ref'>REPO</a></button>
+                    </div>
+                </div>
+
+                <div className='Project ProjectTwo'>
+                    <div>
+                        <h3> Simon</h3>
+                        <br />
+                        <h4> HTML | CSS | JavaScript</h4>
                         <p>
                             A web browser game version of the classic Simon game. Built using HTML, CSS & JS.
                             <br /><br />
@@ -24,9 +40,15 @@ export default function Portfolio() {
                         </p>
                         <button><a href='https://bholeneha.github.io/simon/'>LIVE</a></button> <button><a href='https://github.com/bholeneha/simon'>REPO</a></button>
                     </div>
+                    <div className='SimonImg'>
+                        <img width="500" src={simon} alt="Simon Project Screen Shot" loading="lazy" />
+                    </div>
                 </div>
 
-                <div className='Project ProjectTwo'>
+                <div className='Project ProjectThree'>
+                    <div className='RVImg'>
+                        <img width="500" src={rv} alt="Resource Vault Project Screen Shot" loading="lazy" />
+                    </div>
                     <div>
                         <h3>Resource Vault</h3>
                         <br />
@@ -38,15 +60,9 @@ export default function Portfolio() {
                         </p>
                         <button><a href='https://resource-vault.up.railway.app/'>LIVE</a></button> <button><a href='https://github.com/bholeneha/resource-vault'>REPO</a></button>
                     </div>
-                    <div className='RVImg'>
-                        <img width="500" src={rv} alt="Resource Vault Project Screen Shot" loading="lazy" />
-                    </div>
                 </div>
 
-                <div className='Project ProjectThree'>
-                    <div className='LeafImg'>
-                        <img width="500" src={leafshop} alt="Leaf It To Me Project Screen Shot" loading="lazy" />
-                    </div>
+                <div className='Project ProjectFour'>
                     <div>
                         <h3>Leaf It To Me</h3>
                         <br />
@@ -57,7 +73,10 @@ export default function Portfolio() {
                             <br /><br />
                             An online plant shop that provides users with a wide variety of plants to explore and purchase through a shopping cart system. Built using the MERN stack, it offers a user-friendly interface and including secure user authentication through the use of JWT tokens. Users can easily sign up, log in, and have their carts automatically saved, and additional features to be added to enhance the overall user experience. Leaf it to me is a destination for all plant enthusiasts.
                         </p>
-                        <button><a href='https://leafittome.herokuapp.com/'>LIVE</a></button> <button><a hred='https://leafittome.herokuapp.com/'>REPO</a></button>
+                        <button><a href='https://leafittome.herokuapp.com/'>LIVE</a></button> <button><a href='https://leafittome.herokuapp.com/'>REPO</a></button>
+                    </div>
+                    <div className='LeafImg'>
+                        <img width="500" src={leafshop} alt="Leaf It To Me Project Screen Shot" loading="lazy" />
                     </div>
                 </div>
             </div>
