97b62546a9f3cee82b877edbae21c011600789d5
Neha Bhole <nehasbhole@gmail.com>
2023-04-13 21:09:25 -0400

    changed about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,24 +5,32 @@ export default function About() {
     return (
         <section className='About' id='About'>
             <h1>About</h1>
-            <br />
             <div className='AboutMe'>
                 <div className='HeadShot'>
                     <img src={headshot} alt="Neha Bhole Head Shot" loading="lazy" />
                 </div>
                 <div className='AboutText' >
                     <div>
-                        <h2>Hi, I'm Neha!</h2>
+                        <h2>Hey there, I'm Neha!</h2>
                         <div className='AboutContent'>
-                            <p>Thank you for visiting my portfolio!</p>
+                            <p>Thanks for stopping by my portfolio. I'm a full stack developer based in Toronto, Ontario.</p>
                             <p>
-                                I'm a full stack developer based in Toronto, Ontario. I fell in love with software engineering while tinkering with personal projects on Wordpress, and I haven't looked back since. There's something magical about seeing lines of code come together to create amazing websites and apps, and I love being a part of that process.
+                                Growing up, I never thought I'd end up in tech. In fact, my background is in pediatric behavioral therapy. But life has a funny way of leading you down unexpected paths, and mine led me to software engineering.
                             </p>
                             <p>
-                                When I'm not coding, you can usually find me exploring new hobbies and challenges. I'm currently learning an Indian Classical Dance called Kathak, and I love to experiment with photography. But at the end of the day, web development is my true passion, and I'm always looking for new opportunities to grow and learn.
+                                I fell in love with coding while tinkering with personal projects on WordPress. There's something magical about seeing lines of code come together to create amazing websites and apps, and I knew that this was the field for me.
                             </p>
                             <p>
-                                If you're interested in collaborating or just want to chat about all things web development, feel free to reach out!
+                                Now, I'm a graduate of the Software Engineering Immersive program from General Assembly and the Data Analytics program from the University of Toronto. I've gained proficiency in several programming languages, including HTML, CSS, JavaScript, Python, and SQL.
+                            </p>
+                            <p>
+                                What sets me apart is my unique perspective as someone who's worked in healthcare. My strong empathy, interpersonal skills, and problem-solving abilities have helped me communicate complex technical concepts to a diverse range of stakeholders. I believe that technology has the power to positively impact society in a significant way, and I'm committed to scaling my impact on the community using development.
+                            </p>
+                            <p>
+                                When I'm not coding, I love to explore new hobbies and challenges. I'm currently learning an Indian Classical Dance called Kathak, and I love to experiment with photography. I'm always on the lookout for new opportunities to learn and grow, both personally and professionally.
+                            </p>
+                            <p>
+                                So, if you're interested in collaborating or just want to chat about all things web development, feel free to reach out!
                             </p>
                             {/* Currently Reading, Currently Listening, Currently Learning */}
                         </div>
